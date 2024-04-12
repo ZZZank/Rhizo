78ff54d484da56190b69e8fa257f66aeb8868cb1
ZZZank <3410764033@qq.com>
2024-04-12 18:47:03 +0800

    split different types of remapping method

--- common/src/main/java/dev/latvian/mods/rhino/util/DefaultRemapper.java
+++ common/src/main/java/dev/latvian/mods/rhino/util/DefaultRemapper.java
@@ -1,25 +1,42 @@
 package dev.latvian.mods.rhino.util;
 
 import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Field;
 import java.lang.reflect.Member;
+import java.lang.reflect.Method;
 
+/**
+ * Default impl of {@link Remapper}, will only check {@link RemapForJS} annotation
+ */
 public class DefaultRemapper implements Remapper {
 	public static final DefaultRemapper INSTANCE = new DefaultRemapper();
 
-	private DefaultRemapper() {
+	private DefaultRemapper() {}
+
+	@Override
+	public String getMappedClass(Class<?> from) {
+		RemapForJS remap = from.getAnnotation(RemapForJS.class);
+		if (remap != null) {
+			return remap.value();
+		}
+		return "";
 	}
 
 	@Override
-	public String remap(Class<?> from, Member member) {
-		if (member instanceof AnnotatedElement) {
-			AnnotatedElement annotatedElement = (AnnotatedElement) member;
-			RemapForJS remap = annotatedElement.getAnnotation(RemapForJS.class);
-
-			if (remap != null) {
-				return remap.value();
-			}
+	public String getMappedField(Class<?> from, Field field) {
+		RemapForJS remap = field.getAnnotation(RemapForJS.class);
+		if (remap != null) {
+			return remap.value();
 		}
+		return "";
+	}
 
+	@Override
+	public String getMappedMethod(Class<?> from, Method method) {
+		RemapForJS remap = method.getAnnotation(RemapForJS.class);
+		if (remap != null) {
+			return remap.value();
+		}
 		return "";
 	}
 }
--- common/src/main/java/dev/latvian/mods/rhino/util/Remapper.java
+++ common/src/main/java/dev/latvian/mods/rhino/util/Remapper.java
@@ -1,7 +1,80 @@
 package dev.latvian.mods.rhino.util;
 
+
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Field;
 import java.lang.reflect.Member;
+import java.lang.reflect.Method;
 
 public interface Remapper {
-	String remap(Class<?> from, Member member);
+
+	/**
+	 * @deprecated please use more concrete variants,
+	 * @param from the class that {@code member} belongs to
+	 * @param member a Java member, method/field
+	 * @return remapped name
+	 * @see dev.latvian.mods.rhino.util.Remapper#getMappedField(Class, Field)
+	 * @see dev.latvian.mods.rhino.util.Remapper#getMappedMethod(Class, Method)
+	 */
+	default String remap(Class<?> from, Member member) {
+		if (member instanceof AnnotatedElement annotatedElement) {
+            RemapForJS remap = annotatedElement.getAnnotation(RemapForJS.class);
+			if (remap != null) {
+				return remap.value();
+			}
+		}
+		return "";
+	}
+	static String getTypeName(String type) {
+		int array = 0;
+		while (type.endsWith("[]")) {
+			array++;
+			type = type.substring(0, type.length() - 2);
+		}
+
+		String t = switch (type) {
+			case "boolean" -> "Z";
+			case "byte" -> "B";
+			case "short" -> "S";
+			case "int" -> "I";
+			case "long" -> "J";
+			case "float" -> "F";
+			case "double" -> "D";
+			case "char" -> "C";
+			case "void" -> "V";
+			default -> "L" + type.replace('.', '/') + ";";
+		};
+
+		return array == 0 ? t : ("[".repeat(array) + t);
+	}
+
+	/**
+	 * @return a string holding remapped class name, or an empty string if not remapped
+	 */
+	default String getMappedClass(Class<?> from) {
+		return "";
+	}
+
+	/**
+	 * @param from name of remapped class
+	 * @return the original class name
+	 */
+	default String getUnmappedClass(String from) {
+		return "";
+	}
+
+	/**
+	 * @return a string holding remapped field name, or an empty string if not remapped
+	 */
+	default String getMappedField(Class<?> from, Field field) {
+		return "";
+	}
+
+	/**
+	 * @return a string holding remapped method name, or an empty string if not remapped
+	 */
+	default String getMappedMethod(Class<?> from, Method method) {
+		return "";
+	}
 }
+
