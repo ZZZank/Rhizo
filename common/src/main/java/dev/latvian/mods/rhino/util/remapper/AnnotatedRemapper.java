886b4e8a2c4cf0125cc98ef0a8862dff065fe17b
ZZZank <3410764033@qq.com>
2024-04-12 22:24:44 +0800

    make DefaultRemapper apply both Annotation and Mojmap

---
+++ common/src/main/java/dev/latvian/mods/rhino/util/remapper/AnnotatedRemapper.java
@@ -0,0 +1,40 @@
+package dev.latvian.mods.rhino.util.remapper;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * Default impl of {@link Remapper}, will only check {@link RemapForJS} annotation
+ */
+public class AnnotatedRemapper implements Remapper {
+    public static final AnnotatedRemapper INSTANCE = new AnnotatedRemapper();
+
+    private AnnotatedRemapper() {}
+
+    @Override
+    public String getMappedClass(Class<?> from) {
+        RemapForJS remap = from.getAnnotation(RemapForJS.class);
+        if (remap != null) {
+            return remap.value();
+        }
+        return "";
+    }
+
+    @Override
+    public String getMappedField(Class<?> from, Field field) {
+        RemapForJS remap = field.getAnnotation(RemapForJS.class);
+        if (remap != null) {
+            return remap.value();
+        }
+        return "";
+    }
+
+    @Override
+    public String getMappedMethod(Class<?> from, Method method) {
+        RemapForJS remap = method.getAnnotation(RemapForJS.class);
+        if (remap != null) {
+            return remap.value();
+        }
+        return "";
+    }
+}
--- common/src/main/java/dev/latvian/mods/rhino/util/remapper/DefaultRemapper.java
+++ common/src/main/java/dev/latvian/mods/rhino/util/remapper/DefaultRemapper.java
@@ -1,40 +1,54 @@
 package dev.latvian.mods.rhino.util.remapper;
 
+import dev.latvian.mods.rhino.mod.util.MinecraftRemapper;
+import dev.latvian.mods.rhino.mod.util.RemappingHelper;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 /**
- * Default impl of {@link Remapper}, will only check {@link RemapForJS} annotation
+ * Default impl of {@link Remapper}, will apply {@link AnnotatedRemapper} and {@link MinecraftRemapper},
+ * {@code MinecraftRemapper} is applied only after {@code AnnotatedRemapper} fails to remap
  */
 public class DefaultRemapper implements Remapper {
 	public static final DefaultRemapper INSTANCE = new DefaultRemapper();
+	private final AnnotatedRemapper annotated;
+	private final MinecraftRemapper minecraft;
 
-	private DefaultRemapper() {}
+	private DefaultRemapper() {
+		this.annotated = AnnotatedRemapper.INSTANCE;
+		this.minecraft = RemappingHelper.getMinecraftRemapper();
+	}
+
+	@Override
+	public String getUnmappedClass(String from) {
+		return this.minecraft.getUnmappedClass(from);
+	}
 
 	@Override
 	public String getMappedClass(Class<?> from) {
-		RemapForJS remap = from.getAnnotation(RemapForJS.class);
-		if (remap != null) {
-			return remap.value();
+		String remapped = this.annotated.getMappedClass(from);
+		if (!remapped.isEmpty()) {
+			return remapped;
 		}
-		return "";
+		return this.minecraft.getMappedClass(from);
 	}
 
 	@Override
 	public String getMappedField(Class<?> from, Field field) {
-		RemapForJS remap = field.getAnnotation(RemapForJS.class);
-		if (remap != null) {
-			return remap.value();
+		String remapped = this.annotated.getMappedField(from, field);
+		if (!remapped.isEmpty()) {
+			return remapped;
 		}
-		return "";
+		return this.minecraft.getMappedField(from, field);
 	}
 
 	@Override
 	public String getMappedMethod(Class<?> from, Method method) {
-		RemapForJS remap = method.getAnnotation(RemapForJS.class);
-		if (remap != null) {
-			return remap.value();
+		String remapped = this.annotated.getMappedMethod(from, method);
+		if (!remapped.isEmpty()) {
+			return remapped;
 		}
-		return "";
+		return this.minecraft.getMappedMethod(from, method);
 	}
 }
