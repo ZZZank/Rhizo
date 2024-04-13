2f9f48a2c78c554809721627bd7309cbffe5e807
ZZZank <3410764033@qq.com>
2024-04-13 23:09:48 +0800

    inline AnnotatedRemapper usage
    should be faster

--- common/src/main/java/dev/latvian/mods/rhino/util/remapper/DefaultRemapper.java
+++ common/src/main/java/dev/latvian/mods/rhino/util/remapper/DefaultRemapper.java
@@ -7,16 +7,14 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 /**
- * Default impl of {@link Remapper}, will apply {@link AnnotatedRemapper} and {@link MinecraftRemapper},
- * {@code MinecraftRemapper} is applied only after {@code AnnotatedRemapper} fails to remap
+ * Default impl of {@link Remapper}, will apply {@link RemapForJS} and {@link MinecraftRemapper},
+ * {@link MinecraftRemapper} is applied only after {@link RemapForJS} fails to remap
  */
 public class DefaultRemapper implements Remapper {
     public static final DefaultRemapper INSTANCE = new DefaultRemapper();
-    private final AnnotatedRemapper annotated;
     private final MinecraftRemapper minecraft;
 
     private DefaultRemapper() {
-        this.annotated = AnnotatedRemapper.INSTANCE;
         this.minecraft = RemappingHelper.getMinecraftRemapper();
     }
 
@@ -27,11 +25,11 @@ public class DefaultRemapper implements Remapper {
 
     @Override
     public String getMappedClass(Class<?> from) {
-        String remapped = this.annotated.getMappedClass(from);
-        if (!remapped.isEmpty()) {
-            return remapped;
+        RemapForJS annot = from.getAnnotation(RemapForJS.class);
+        if (annot != null) {
+            return annot.value();
         }
-        remapped = this.minecraft.getMappedClass(from);
+        var remapped = this.minecraft.getMappedClass(from);
         if (!remapped.isEmpty()) {
             return remapped;
         }
@@ -40,11 +38,11 @@ public class DefaultRemapper implements Remapper {
 
     @Override
     public String getMappedField(Class<?> from, Field field) {
-        String remapped = this.annotated.getMappedField(from, field);
-        if (!remapped.isEmpty()) {
-            return remapped;
+        RemapForJS annot = field.getAnnotation(RemapForJS.class);
+        if (annot != null) {
+            return annot.value();
         }
-        remapped = this.minecraft.getMappedField(from, field);
+        var remapped = this.minecraft.getMappedField(from, field);
         if (!remapped.isEmpty()) {
             return remapped;
         }
@@ -53,11 +51,11 @@ public class DefaultRemapper implements Remapper {
 
     @Override
     public String getMappedMethod(Class<?> from, Method method) {
-        String remapped = this.annotated.getMappedMethod(from, method);
-        if (!remapped.isEmpty()) {
-            return remapped;
+        RemapForJS annot = method.getAnnotation(RemapForJS.class);
+        if (annot != null) {
+            return annot.value();
         }
-        remapped = this.minecraft.getMappedMethod(from, method);
+        var remapped = this.minecraft.getMappedMethod(from, method);
         if (!remapped.isEmpty()) {
 			return remapped;
         }
