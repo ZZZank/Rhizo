ea03b964a12768cb3c2596d2ad49b0b0e00acd86
ZZZank <3410764033@qq.com>
2024-04-18 21:25:37 +0800

    rollback classgen name

--- common/src/main/java/dev/latvian/mods/rhino/optimizer/Codegen.java
+++ common/src/main/java/dev/latvian/mods/rhino/optimizer/Codegen.java
@@ -65,13 +65,13 @@ public class Codegen implements Evaluator {
 
         String baseName = "c";
         if (!tree.getSourceName().isEmpty()) {
-            baseName = tree.getSourceName().replaceAll("\\W", "$");
+            baseName = tree.getSourceName().replaceAll("\\W", "_");
             if (!Character.isJavaIdentifierStart(baseName.charAt(0))) {
                 baseName = "_" + baseName;
             }
         }
 
-        String mainClassName = "dev.latvian.mods.rhino.gen." + baseName + "$" + serial;
+        String mainClassName = "dev.latvian.mods.rhino.gen." + baseName + "_" + serial;
 
         byte[] mainClassBytes = compileToClassFile(compilerEnv, mainClassName,
             tree, encodedSource,
