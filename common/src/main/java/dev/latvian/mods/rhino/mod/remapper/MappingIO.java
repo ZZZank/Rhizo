1d4a89431b6a30993672a485a40ec76203202e11
ZZZank <3410764033@qq.com>
2024-05-25 17:03:26 +0800

    finish Rhizo mapping writing, make Exceptions thrown by MappingIO more concise

--- common/src/main/java/dev/latvian/mods/rhino/mod/remapper/MappingIO.java
+++ common/src/main/java/dev/latvian/mods/rhino/mod/remapper/MappingIO.java
@@ -2,28 +2,14 @@ package dev.latvian.mods.rhino.mod.remapper;
 
 import dev.latvian.mods.rhino.util.remapper.RemapperException;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 
 public abstract class MappingIO {
 
-    public interface Handler<T> {
-        /**
-         * write data into stream. Implementation of this should write enough data for {@link Handler#read(InputStream)}
-         * to rebuild the object itself
-         */
-        void write(OutputStream stream);
-
-        /**
-         * read data from stream, and build a new object from these data. Implementation of this should consume all written
-         * data of such type, and should NOT return caller of this method
-         * @return a NEW object containing data fetched from stream
-         */
-        T read(InputStream stream);
-    }
-
-    public static void writeVarInt(OutputStream stream, int value) throws Exception {
+    public static void writeVarInt(OutputStream stream, int value) throws IOException {
         while ((value & -128) != 0) {
             stream.write(value & 127 | 128);
             value >>>= 7;
@@ -32,13 +18,13 @@ public abstract class MappingIO {
         stream.write(value);
     }
 
-    public static void writeUtf(OutputStream stream, String value) throws Exception {
+    public static void writeUtf(OutputStream stream, String value) throws IOException {
         byte[] bytes = value.getBytes(StandardCharsets.UTF_8);
         writeVarInt(stream, bytes.length);
         stream.write(bytes);
     }
 
-    public static int readVarInt(InputStream stream) throws Exception {
+    public static int readVarInt(InputStream stream) throws IOException {
         int i = 0;
         int j = 0;
 
@@ -54,7 +40,7 @@ public abstract class MappingIO {
         return i;
     }
 
-    public static String readUtf(InputStream stream) throws Exception {
+    public static String readUtf(InputStream stream) throws IOException {
         byte[] bytes = new byte[readVarInt(stream)];
 
         for (int i = 0; i < bytes.length; i++) {
--- common/src/main/java/dev/latvian/mods/rhino/mod/remapper/RhizoMappingLoader.java
+++ common/src/main/java/dev/latvian/mods/rhino/mod/remapper/RhizoMappingLoader.java
@@ -8,16 +8,19 @@ import dev.latvian.mods.rhino.util.remapper.RemapperException;
 import net.neoforged.srgutils.IMappingFile;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.net.URLConnection;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.zip.GZIPOutputStream;
 
-public abstract class RhizoMapping {
+public abstract class RhizoMappingLoader {
+
+    //the number 31, is obtained by running SHA256 on string "lambda", and calculating `HASH mod 128`
+    private static final String SKIP_MARK = "31";
 
     /**
-     * generate a mapping file called "mm.jsmappings", can provide conversion between raw name,
+     * generate a mapping file called "mm.jsmappings", that can provides name conversion between
      * in-game name(srg name) and mapped name.
      * <p>
      * Official mapping provides raw name <-> mapped name conversion, and the param {@code callback}
@@ -39,39 +42,63 @@ public abstract class RhizoMapping {
             //in-game -> mapped
             var target = vanillaMapping.chain(nativeMapping).reverse();
             //write mapping
-            try (var out = new GZIPOutputStream(Files.newOutputStream(JavaPortingHelper.ofPath("mm.jsmappings")))) {
-                var classes = target.getClasses();
-                //TODO: do not write lambda
-                MappingIO.writeVarInt(out, classes.size());
-                for (IMappingFile.IClass clazz : classes) {
-                    MappingIO.writeUtf(out, clazz.getOriginal());
-                    MappingIO.writeUtf(out, clazz.getMapped());
-                    //method
-                    var methods = clazz.getMethods();
-                    MappingIO.writeVarInt(out, methods.size());
-                    for (IMappingFile.IMethod method : methods) {
-                        //TODO
-                        method.getDescriptor();
-                    }
-                    //field
-                    var fields = clazz.getFields();
-                    MappingIO.writeVarInt(out, fields.size());
-                    for (IMappingFile.IField field : fields) {
-                        var desc = field.getDescriptor();
-                        MappingIO.writeUtf(out, desc == null ? "" : desc);
-                        desc = field.getMappedDescriptor();
-                        MappingIO.writeUtf(out, desc == null ? "" : desc);
-                    }
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            writeRhizoMapping(JavaPortingHelper.ofPath("mm.jsmappings"), target);
         } catch (IOException e) {
             RemappingHelper.LOGGER.error("Mapping generation failed");
             e.printStackTrace();
             return;
         }
         RemappingHelper.LOGGER.info("Finished generating mappings!");
+    }
+
+    private static void writeRhizoMapping(Path path, IMappingFile mapping) throws IOException {
+        try (var out = new GZIPOutputStream(Files.newOutputStream(path))) {
+            //used for auto fetching skip mark when parsing
+            MappingIO.writeUtf(out, SKIP_MARK);
+            //class
+            var classes = mapping.getClasses();
+            MappingIO.writeVarInt(out, classes.size());
+            for (IMappingFile.IClass clazz : classes) {
+                if (isAnonymousClass(clazz.getMapped())) {
+                    MappingIO.writeUtf(out, SKIP_MARK);
+                    continue;
+                }
+                MappingIO.writeUtf(out, clazz.getOriginal());
+                MappingIO.writeUtf(out, clazz.getMapped());
+                //method
+                var methods = clazz.getMethods();
+                MappingIO.writeVarInt(out, methods.size());
+                for (IMappingFile.IMethod method : methods) {
+                    if (method.getMapped().startsWith("lambda$")) {
+                        MappingIO.writeUtf(out, SKIP_MARK);
+                        continue;
+                    }
+                    MappingIO.writeUtf(out, method.getDescriptor());
+                    MappingIO.writeUtf(out, method.getMapped());
+                }
+                //field
+                var fields = clazz.getFields();
+                MappingIO.writeVarInt(out, fields.size());
+                for (IMappingFile.IField field : fields) {
+                    var desc = field.getOriginal();
+                    MappingIO.writeUtf(out, desc == null ? "" : desc);
+                    desc = field.getMapped();
+                    MappingIO.writeUtf(out, desc == null ? "" : desc);
+                }
+            }
+        }
+    }
+
+    /**
+     * @param className the one you can get via {@link Class#getName()}
+     * @return true if the class that the name represents is an anonymous class
+     */
+    private static boolean isAnonymousClass(final String className) {
+        var lastIndex = className.lastIndexOf('$');
+        if (lastIndex < 0) {
+            return false;
+        }
+        return className.substring(lastIndex + 1).chars().allMatch(c -> c >= '0' && c <= '9');
     }
 
     /**
@@ -92,8 +119,7 @@ public abstract class RhizoMapping {
             }
         }
         if (verInfo == null) {
-            throw new RemapperException(String.format(
-                "No version information for '%s' from official source",
+            throw new RemapperException(String.format("No version information for '%s' from official source",
                 mcVersion
             ));
         }
@@ -102,8 +128,7 @@ public abstract class RhizoMapping {
         try (var metaReader = RemappingHelper.createUrlReader(verInfo.get("url").getAsString())) {
             var meta = JsonUtils.GSON.fromJson(metaReader, JsonObject.class);
             if (!(meta.get("downloads") instanceof JsonObject o)
-                || !(o.get("client_mappings") instanceof JsonObject cmap)
-                || !cmap.has("url")) {
+                || !(o.get("client_mappings") instanceof JsonObject cmap) || !cmap.has("url")) {
                 throw new RemapperException("This Minecraft version doesn't have mappings!");
             }
             mappingUrl = RemappingHelper.getUrlConnection(cmap.get("url").getAsString());
