901ffe523fe19a813b84542a91313951fa2b446b
ZZZank <3410764033@qq.com>
2024-05-27 20:50:47 +0800

    move VMBridge_jdk18 to inside VMBridge.java
    should be safe, since all members are protected

--- common/src/main/java/dev/latvian/mods/rhino/VMBridge.java
+++ common/src/main/java/dev/latvian/mods/rhino/VMBridge.java
@@ -8,10 +8,7 @@
 
 package dev.latvian.mods.rhino;
 
-import dev.latvian.mods.rhino.jdk18.VMBridge_jdk18;
-
-import java.lang.reflect.AccessibleObject;
-import java.lang.reflect.Method;
+import java.lang.reflect.*;
 
 public abstract class VMBridge {
 
@@ -19,7 +16,7 @@ public abstract class VMBridge {
 
 	private static VMBridge makeVMInstance() {
 		/*
-		String[] classNames = {"dev.latvian.mods.rhino.VMBridge_custom", "dev.latvian.mods.rhino.jdk18.VMBridge_jdk18",};
+		String[] classNames = {"dev.latvian.mods.rhino.VMBridge_custom", "dev.latvian.mods.rhino.VMBridge.VMBridge_jdk18",};
 		for (int i = 0; i != classNames.length; ++i) {
 			String className = classNames[i];
 			Class<?> cl = Kit.classOrNull(className);
@@ -102,4 +99,111 @@ public abstract class VMBridge {
 	 *                    {@link #getInterfaceProxyHelper(ContextFactory, Class[])}.
 	 */
 	protected abstract Object newInterfaceProxy(Object proxyHelper, ContextFactory cf, InterfaceAdapter adapter, Object target, Scriptable topScope);
+
+	/**
+	 * VMBridge implemented targeting Java 8
+	 */
+	public static class VMBridge_jdk18 extends VMBridge {
+		private static final ThreadLocal<Object[]> contextLocal = new ThreadLocal<>();
+
+		@Override
+		protected Object getThreadContextHelper() {
+			// To make subsequent batch calls to getContext/setContext faster
+			// associate permanently one element array with contextLocal
+			// so getContext/setContext would need just to read/write the first
+			// array element.
+			// Note that it is necessary to use Object[], not Context[] to allow
+			// garbage collection of Rhino classes. For details see comments
+			// by Attila Szegedi in
+			// https://bugzilla.mozilla.org/show_bug.cgi?id=281067#c5
+
+			Object[] storage = contextLocal.get();
+			if (storage == null) {
+				storage = new Object[1];
+				contextLocal.set(storage);
+			}
+			return storage;
+		}
+
+		@Override
+		protected Context getContext(Object contextHelper) {
+			Object[] storage = (Object[]) contextHelper;
+			return (Context) storage[0];
+		}
+
+		@Override
+		protected void setContext(Object contextHelper, Context cx) {
+			Object[] storage = (Object[]) contextHelper;
+			storage[0] = cx;
+		}
+
+		@Override
+		protected boolean tryToMakeAccessible(AccessibleObject accessible) {
+			if (accessible.isAccessible()) {
+				return true;
+			}
+
+			try {
+				accessible.setAccessible(true);
+			} catch (Exception ex) {}
+
+			return accessible.isAccessible();
+		}
+
+		@Override
+		protected Object getInterfaceProxyHelper(ContextFactory cf, Class<?>[] interfaces) {
+			// XXX: How to handle interfaces array withclasses from different
+			// class loaders? Using cf.getApplicationClassLoader() ?
+			ClassLoader loader = interfaces[0].getClassLoader();
+			Class<?> cl = Proxy.getProxyClass(loader, interfaces);
+			Constructor<?> c;
+			try {
+				c = cl.getConstructor(InvocationHandler.class);
+			} catch (NoSuchMethodException ex) {
+				// Should not happen
+				throw new IllegalStateException(ex);
+			}
+			return c;
+		}
+
+		@Override
+		protected Object newInterfaceProxy(Object proxyHelper, final ContextFactory cf, final InterfaceAdapter adapter, final Object target, final Scriptable topScope) {
+			Constructor<?> c = (Constructor<?>) proxyHelper;
+
+			InvocationHandler handler = (proxy, method, args) -> {
+				// In addition to methods declared in the interface, proxies
+				// also route some java.lang.Object methods through the
+				// invocation handler.
+				if (method.getDeclaringClass() == Object.class) {
+					String methodName = method.getName();
+					switch (methodName) {
+						case "equals" -> {
+							// Note: we could compare a proxy and its wrapped function
+							// as equal here but that would break symmetry of equal().
+							// The reason == suffices here is that proxies are cached
+							// in ScriptableObject (see NativeJavaObject.coerceType())
+							return proxy == args[0];
+						}
+						case "hashCode" -> {
+							return target.hashCode();
+						}
+						case "toString" -> {
+							return "Proxy[" + target.toString() + "]";
+						}
+					}
+				}
+				return adapter.invoke(cf, target, topScope, proxy, method, args);
+			};
+			Object proxy;
+			try {
+				proxy = c.newInstance(handler);
+			} catch (InvocationTargetException ex) {
+				throw Context.throwAsScriptRuntimeEx(ex);
+			} catch (IllegalAccessException | InstantiationException ex) {
+				// Should not happen
+				throw new IllegalStateException(ex);
+			}
+			return proxy;
+		}
+	}
 }
