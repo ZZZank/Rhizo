9178ccd67a45c3abc42fe0bd53d44551db0ab048
ZZZank <3410764033@qq.com>
2024-06-15 18:54:30 +0800

    a little bit more accurate JSParam annotation

--- common/src/main/java/dev/latvian/mods/rhino/annotations/typing/JSParam.java
+++ common/src/main/java/dev/latvian/mods/rhino/annotations/typing/JSParam.java
@@ -1,8 +1,12 @@
 package dev.latvian.mods.rhino.annotations.typing;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
 /**
  * @author ZZZank
  */
+@Target(ElementType.METHOD)
 public @interface JSParam {
     String rename() default "";
     String type();
--- common/src/main/java/dev/latvian/mods/rhino/annotations/typing/JSParams.java
+++ common/src/main/java/dev/latvian/mods/rhino/annotations/typing/JSParams.java
@@ -2,9 +2,11 @@ package dev.latvian.mods.rhino.annotations.typing;
 
 /**
  * note: ordinal
+ * <p>
+ * use null to skip a param
  * @author ZZZank
  */
+@JSInfo("Note: ordinal. Use null to skip a param")
 public @interface JSParams {
-    String[] renames() default {};
-    String[] types();
+    JSParam[] params() default {};
 }
