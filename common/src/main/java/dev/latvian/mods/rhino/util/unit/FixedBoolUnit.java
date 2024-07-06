9a2959001420b92db807fd71b25832e35109d42a
ZZZank <3410764033@qq.com>
2024-07-06 13:07:00 +0800

    FixedBoolUnit

---
+++ common/src/main/java/dev/latvian/mods/rhino/util/unit/FixedBoolUnit.java
@@ -0,0 +1,41 @@
+package dev.latvian.mods.rhino.util.unit;
+
+/**
+ * @author ZZZank
+ */
+public class FixedBoolUnit extends FixedUnit {
+    public static final FixedBoolUnit TRUE = new FixedBoolUnit(true);
+    public static final FixedBoolUnit FALSE = new FixedBoolUnit(false);
+
+    private final boolean b;
+
+    protected FixedBoolUnit(boolean b) {
+        super(b ? 1F : 0F);
+        this.b = b;
+    }
+
+    @Override
+    public float get() {
+        return b ? 1F : 0F;
+    }
+
+    @Override
+    public boolean getAsBoolean() {
+        return this.b;
+    }
+
+    @Override
+    public int getAsInt() {
+        return b ? 1 : 0;
+    }
+
+    @Override
+    public Unit toBool() {
+        return this;
+    }
+
+    @Override
+    public void append(StringBuilder sb) {
+        sb.append(b);
+    }
+}
