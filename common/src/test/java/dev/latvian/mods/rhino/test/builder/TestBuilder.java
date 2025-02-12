c10a1b41aab80fa574dddbe54d8c6acbda5e8266
ZZZank <3410764033@qq.com>
2025-02-12 10:13:30 +0800

    builder-based modular test setup

---
+++ common/src/test/java/dev/latvian/mods/rhino/test/builder/TestBuilder.java
@@ -0,0 +1,73 @@
+package dev.latvian.mods.rhino.test.builder;
+
+import dev.latvian.mods.rhino.Context;
+import dev.latvian.mods.rhino.ContextFactory;
+import dev.latvian.mods.rhino.Scriptable;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import lombok.val;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+/**
+ * @author ZZZank
+ */
+@Setter
+@Accessors(chain = true)
+public class TestBuilder {
+
+    @NotNull
+    public String script;
+    @NotNull
+    public String name = "test";
+    public int lineNumber = 0;
+
+    @NotNull
+    public Supplier<ContextFactory> factory = ContextFactory::new;
+    @Nullable
+    public Consumer<ContextFactory> factoryModifier;
+    @Nullable
+    public Object securityDomain;
+
+    @NotNull
+    public Function<ContextFactory, Context> context = ContextFactory::enterContext;
+    @Nullable
+    public BiConsumer<ContextFactory, Context> contextModifier;
+
+    @NotNull
+    public Function<Context, Scriptable> scope = Context::initStandardObjects;
+    @Nullable
+    public BiConsumer<Context, Scriptable> scopeModifier;
+
+    public static TestBuilder of() {
+        return new TestBuilder();
+    }
+
+    public static TestBuilder script(String script) {
+        return new TestBuilder().setScript(script);
+    }
+
+    public void run() {
+        val factory = this.factory.get();
+        if (factoryModifier != null) {
+            factoryModifier.accept(factory);
+        }
+
+        val context = this.context.apply(factory);
+        if (contextModifier != null) {
+            contextModifier.accept(factory, context);
+        }
+
+        val scope = this.scope.apply(context);
+        if (scopeModifier != null) {
+            scopeModifier.accept(context, scope);
+        }
+
+        context.evaluateString(scope, script, name, lineNumber, securityDomain);
+    }
+}
