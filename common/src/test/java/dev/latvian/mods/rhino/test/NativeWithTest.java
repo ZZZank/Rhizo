d0943b286bd28e21850f4f5be03c882449c7c125
ZZZank <3410764033@qq.com>
2025-03-01 15:47:23 +0800

    test++

--- common/src/test/java/dev/latvian/mods/rhino/test/NativeWithTest.java
+++ common/src/test/java/dev/latvian/mods/rhino/test/NativeWithTest.java
@@ -1,28 +1,39 @@
 package dev.latvian.mods.rhino.test;
 
 import dev.latvian.mods.rhino.test.impl.base.RhinoTest;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
  * @author ZZZank
  */
+@Disabled
 public class NativeWithTest {
     public static final RhinoTest TEST = new RhinoTest("with");
+
+	@Test
+	void basic() {
+		TEST.test("basic", """
+			with (console) {
+				log("aaa")
+			}""", """
+			aaa""");
+	}
 
     @Test
     public void dontError() {
         TEST.test("mixed", """
-			const exampleArray = [1, "2", 3, false]
+			const exampleArray = [undefined, 1, "2", 3, false]
 			
 			const someFn = (o) => {
 				if (o == undefined) {
-					return false
+					return true
 				}
-				return typeof o == "boolean"
+				return typeof o != "boolean"
 			}
 			
 			for (let elem in exampleArray) {
-				if (!someFn(elem)) {
+				if (someFn(elem)) {
 					continue
 				}
 				console.log(elem)
