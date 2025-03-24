a8892a02a18f18088ee6825c69079e6c382f38e7
ZZZank <3410764033@qq.com>
2025-03-24 23:09:21 +0800

    test++

--- common/src/test/java/dev/latvian/mods/rhino/test/NullishCoalescingTests.java
+++ common/src/test/java/dev/latvian/mods/rhino/test/NullishCoalescingTests.java
@@ -3,9 +3,12 @@ package dev.latvian.mods.rhino.test;
 import dev.latvian.mods.rhino.test.impl.base.RhinoTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 @SuppressWarnings("unused")
 public class NullishCoalescingTests {
-	public static final RhinoTest TEST = new RhinoTest("nullishCoalescing");
+	public static final RhinoTest TEST = new RhinoTest("nullishCoalescing")
+        .withBinding("list", t -> new ArrayList<>());
 
 	@Test
 	public void bothNonNull() {
@@ -78,4 +81,16 @@ public class NullishCoalescingTests {
 			10
 			""");
 	}
+
+    @Test
+    public void evalOnce() {
+        TEST.test("evalOnce", """
+            console.log(123 ?? list.add("huh"))
+            console.log(null ?? list.add("oh"))
+            console.log(list.size())
+            """, """
+            123
+            true
+            1""");
+    }
 }
