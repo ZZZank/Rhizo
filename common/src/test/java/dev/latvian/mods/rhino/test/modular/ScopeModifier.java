cd743e856b33c34b968768c34b2201a8a5d1626c
ZZZank <3410764033@qq.com>
2025-03-27 15:43:11 +0800

    add context param for ScopeModifier

--- common/src/test/java/dev/latvian/mods/rhino/test/modular/ScopeModifier.java
+++ common/src/test/java/dev/latvian/mods/rhino/test/modular/ScopeModifier.java
@@ -1,5 +1,6 @@
 package dev.latvian.mods.rhino.test.modular;
 
+import dev.latvian.mods.rhino.Context;
 import dev.latvian.mods.rhino.NativeJavaClass;
 import dev.latvian.mods.rhino.Scriptable;
 import dev.latvian.mods.rhino.ScriptableObject;
@@ -10,29 +11,32 @@ import dev.latvian.mods.rhino.ScriptableObject;
 public interface ScopeModifier extends Snapshot.Modifier {
 
     static ScopeModifier none() {
-        return scope -> {};
+        return (cx, scope) -> {};
     }
 
     static ScopeModifier putProperty(String name, Object value) {
-        return scope -> ScriptableObject.putProperty(scope, name, value);
+        return (cx, scope) ->
+            ScriptableObject.putProperty(scope, name, Context.javaToJS(cx, value, scope));
     }
 
     static ScopeModifier putConstProperty(String name, Object value) {
-        return scope -> ScriptableObject.putConstProperty(scope, name, value);
+        return (cx, scope) ->
+            ScriptableObject.putConstProperty(scope, name, Context.javaToJS(cx, value, scope));
     }
 
     static ScopeModifier importClass(String name, Class<?> clazz) {
-        return scope -> ScriptableObject.putProperty(scope, name, new NativeJavaClass(scope, clazz));
+        return (cx, scope) ->
+            ScriptableObject.putProperty(scope, name, new NativeJavaClass(cx, scope, clazz));
     }
 
     static ScopeModifier importClass(Class<?> clazz) {
         return importClass(clazz.getSimpleName(), clazz);
     }
 
-    void modify(Scriptable scope);
+    void modify(Context cx, Scriptable scope);
 
     @Override
     default void modify(Snapshot snapshot) {
-        modify(snapshot.scope());
+        modify(snapshot.context(), snapshot.scope());
     }
 }
