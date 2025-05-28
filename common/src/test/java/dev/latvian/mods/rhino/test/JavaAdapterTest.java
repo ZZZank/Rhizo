eafd03f1f3db4df404be45c05a4cd60413de787b
ZZZank <3410764033@qq.com>
2025-05-28 14:13:22 +0800

    fix value wrapping for JavaAdapter

--- common/src/test/java/dev/latvian/mods/rhino/test/JavaAdapterTest.java
+++ common/src/test/java/dev/latvian/mods/rhino/test/JavaAdapterTest.java
@@ -2,6 +2,10 @@ package dev.latvian.mods.rhino.test;
 
 import dev.latvian.mods.rhino.test.impl.ExampleAbstractClass;
 import dev.latvian.mods.rhino.test.impl.base.RhinoTest;
+import lombok.val;
+import org.apache.commons.lang3.mutable.Mutable;
+import org.apache.commons.lang3.mutable.MutableObject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.function.Supplier;
@@ -12,7 +16,8 @@ import java.util.function.Supplier;
 public class JavaAdapterTest {
     private static final RhinoTest TEST = new RhinoTest("JavaAdapterTest")
         .withBinding(Supplier.class.getSimpleName(), t -> Supplier.class)
-        .withBinding("Abstract", t -> ExampleAbstractClass.class);
+        .withBinding("Abstract", t -> ExampleAbstractClass.class)
+        .withBinding(ExampleInt2StrFunction.class.getSimpleName(), t -> ExampleInt2StrFunction.class);
 
     @Test
     public void fnInterface() {
@@ -38,5 +43,26 @@ public class JavaAdapterTest {
             });
             testSupplier.get();
             """, "triggered");
+    }
+
+    public interface ExampleInt2StrFunction {
+        Mutable<ExampleInt2StrFunction> INSTANCE = new MutableObject<>();
+
+        String apply(int i);
+    }
+
+    @Test
+    public void wrap() {
+        TEST.test("simple", """
+            const test = new JavaAdapter(ExampleInt2StrFunction, {
+                apply: function(i) {
+                    // Rhino should be able to wrap number to string automatically
+                    return i;
+                }
+            });
+            ExampleInt2StrFunction.INSTANCE.setValue(test);
+            """, "");
+        val s = ExampleInt2StrFunction.INSTANCE.getValue().apply(123);
+        Assertions.assertEquals("123", s);
     }
 }
